17fd04e8ecb984d7aecc1bc2825aa699fed9f980
Valeri Mladenov <valeri.mladenov10@gmail.com>
2020-09-06 20:12:25 +0300

    🚚 First Commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { ThemeProvider } from "theme-ui";
+import theme from "./theme";
+import { Parallax } from "react-spring/renderprops-addons";
+import Layout from "./components/Layout";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <Layout>
+      <Parallax pages={5}>
+        <Hero offset={0} factor={1} />
+        <Projects offset={1} factor={2} />
+        <About offset={3} factor={1} />
+        <Contact offset={4} factor={1} />
+      </Parallax>
+    </Layout>
+  </ThemeProvider>
+);
+
+export default App;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+/** @jsx jsx */
+import { Box, jsx } from "theme-ui";
+
+const Footer = () => {
+  return (
+    <Box as="footer" variant="footer">
+      Copyright &copy; Valeri Mladenov. All rights reserved.
+    </Box>
+  );
+};
+
+export default Footer;
